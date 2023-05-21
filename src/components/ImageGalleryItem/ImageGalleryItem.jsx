0de8a217eb12856d997f0961fe060361655423a2
Anna Kravets <annakravets1906@gmail.com>
2023-05-21 16:52:32 +0300

    Added hooks to Modal

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,40 +1,34 @@
 // import PropTypes from 'prop-types';
-import { Component } from 'react';
+import { useState } from 'react';
 import Modal from 'components/Modal/Modal';
 import {
   ImageGalleryItemEl,
   ImageGalleryItemImage,
 } from './ImageGalleryItemStyled';
 
-export default class ImageGalleryItem extends Component {
-  state = {
-    showModal: false,
+export default function ImageGalleryItem({ img, largeImg, tags }) {
+  const [showModal, setShowModal] = useState(false);
+
+  const openModal = () => {
+    setShowModal(true);
   };
 
-  openModal = () => {
-    this.setState({ showModal: true });
+  const closeModal = () => {
+    setShowModal(false);
   };
 
-  closeModal = () => {
-    this.setState({ showModal: false });
-  };
-
-  render() {
-    const { showModal } = this.state;
-    const { img, largeImg, tags } = this.props;
-    return (
-      <>
-        <ImageGalleryItemEl onClick={this.openModal}>
-          <ImageGalleryItemImage src={img} alt={tags} />
-        </ImageGalleryItemEl>
-        {showModal && (
-          <Modal onClose={this.closeModal}>
-            <img src={largeImg} alt={tags} />
-          </Modal>
-        )}
-      </>
-    );
-  }
+  return (
+    <>
+      <ImageGalleryItemEl onClick={openModal}>
+        <ImageGalleryItemImage src={img} alt={tags} />
+      </ImageGalleryItemEl>
+      {showModal && (
+        <Modal onClose={closeModal}>
+          <img src={largeImg} alt={tags} />
+        </Modal>
+      )}
+    </>
+  );
 }
 
 // ImageGalleryItem.propTypes = {
